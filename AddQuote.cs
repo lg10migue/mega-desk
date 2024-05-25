9c61913417acb02e9e15db4b365597535de77c75
DiegoArrC <castanedadiego12.dac@gmail.com>
2024-05-24 20:37:26 -0700

    Added json interpreter, bug with grabbing material price "rebase"

--- AddQuote.cs
+++ AddQuote.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace megaDesk
 {
@@ -67,6 +68,21 @@ namespace megaDesk
             Desk.Material selectedMat = (Desk.Material)material.SelectedValue;
             Desk desk = new Desk(Convert.ToInt32(deskWidth.Text), Convert.ToInt32(deskDepth.Text), Convert.ToInt32(drawerCount.Text), selectedMat);
             DeskQuote quote = new DeskQuote(customerName.Text, desk, rush.SelectedItem.ToString(), DateTime.Now);
+
+            string filePath = @"quotes.json";
+            List<DeskQuote> quotes = new List<DeskQuote>();
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                quotes = JsonConvert.DeserializeObject<List<DeskQuote>>(json);
+            }
+
+            quotes.Add(quote);
+
+            string updatedJson = JsonConvert.SerializeObject(quotes, Formatting.Indented);
+
+            File.WriteAllText(filePath, updatedJson);
 
             DisplayQuote screen = new DisplayQuote(quote._quoteDate.ToString(), quote._quote.ToString(), quote._customerName, quote._productionTime.ToString());
             screen.Tag = this;
